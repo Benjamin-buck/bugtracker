fd6517068ed147cf1fbd6477256e6cbeb11c10a1
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-10-17 13:34:05 -0400

    Build an API for updating issues.

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,14 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import prisma from "@/prisma/client";
-import { createIssueSchema } from "../../ValidationSchema";
+import { issueSchema } from "../../ValidationSchema";
 
 export async function POST(request: NextRequest) {
   // Gets the body of the request
   const body = await request.json();
 
   // Runs ZOD validation checks on the body
-  const validation = createIssueSchema.safeParse(body);
+  const validation = issueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
 
