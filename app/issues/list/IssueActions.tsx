0ee9b86dcdbf25dbe9a2803b3488fbf23abfcecf
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-10-18 12:00:51 -0400

    Adds filter select

--- app/issues/list/IssueActions.tsx
+++ app/issues/list/IssueActions.tsx
@@ -1,13 +1,15 @@
-import { Button } from "@radix-ui/themes";
+import { Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
+import IssueStatusFilter from "./IssueStatusFilter";
 
 const IssueActions = () => {
   return (
-    <div>
+    <Flex mb="5" justify="between">
       <Button>
         <Link href="/issues/new">New Issue</Link>
       </Button>
-    </div>
+      <IssueStatusFilter />
+    </Flex>
   );
 };
 
