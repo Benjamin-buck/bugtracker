6a4e2bfd06b1c31ad44817d97ad8dccb1a14331d
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-10-23 14:26:30 -0400

    populate the assignee field

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,15 +1,32 @@
 "use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  // Use effect hook to call the back-end
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
   return (
     <Select.Root defaultValue="apple">
+      {/* @ts-ignore */}
       <Select.Trigger placeholder="Select..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="orange">Joe Rogan</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
