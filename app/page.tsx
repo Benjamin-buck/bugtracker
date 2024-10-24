aea853f8de553ddde4bc466b8486e5fd29f9109f
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-10-24 17:29:36 -0400

    build the layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
-  return <div>hello world</div>;
+  return <Pagination itemCount={15} pageSize={10} currentPage={1}></Pagination>;
 }
